9de29e72301890fe2db4c87bdd36bbbd1f24e5a4
Coulson <ckgathi@bhp.org.bw>
2016-12-20 12:32:21 +0200

    update forms

--- household/tests/test_household_structure_form.py
+++ household/tests/test_household_structure_form.py
@@ -10,8 +10,5 @@ class TestHouseholdStructureForm(HouseholdMixin, TestCase):
         self.make_household_with_household_log_entry()
         self.options = {}
 
-    def test_enumerations_attemptsyes(self):
-        self.options.update()
-        infant_birth_exam = InfantBirthExamForm(data=self.options)
-        errors = ''.join(infant_birth_exam.errors.get('__all__'))
-        self.assertIn(u'If abnormal, please specify.', errors)
+    def test_enumerations_attempts(self):
+        pass
