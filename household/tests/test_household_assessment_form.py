75ae2e11a15d7debb82148f268ce4cdb801362a3
Coulson <ckgathi@bhp.org.bw>
2016-12-21 07:25:03 +0200

    remove duplicate tests

--- household/tests/test_household_assessment_form.py
+++ household/tests/test_household_assessment_form.py
@@ -32,28 +32,3 @@ class TestHouseholdAssessmentForm(HouseholdMixin, TestCase):
         self.assertFalse(form.is_valid())
         errors = ''.join(form.errors.get('__all__'))
         self.assertIn('Question 2 must be answered when question 1 answer is Yes.', errors)
-
-    def test_enumerations_attempts(self):
-        """Assert that if 3 enumeration attempts are not yet made household assessment form cannot be filled."""
-        self.make_household_log_entry(household_log=self.household_log, household_status=NO_HOUSEHOLD_INFORMANT)
-        self.assertEqual(self.household_structure.enumeration_attempts, 2)
-        self.data.update(eligibles_last_seen_home=UNKNOWN_OCCUPIED)
-        form = HouseholdAssessmentForm(data=self.data)
-        self.assertFalse(form.is_valid())
-        errors = ''.join(form.errors.get('__all__'))
-        self.assertIn('Form is not required, yet. Three enumeration attempts are required before Household Residency '
-                      'Status is required. Got enumeration_attempts=2, last_log_status=True, '
-                      'failed_enumeration_attempts=2', errors)
-
-    def test_household_already_enumerated(self):
-        """Assert that a houselhod is already enumerated when adding household assessment."""
-        self.make_household_log_entry(household_log=self.household_log, household_status=NO_HOUSEHOLD_INFORMANT)
-        self.make_household_log_entry(household_log=self.household_log, household_status=NO_HOUSEHOLD_INFORMANT)
-        self.assertEqual(self.household_structure.enumeration_attempts, 3)
-        self.data.update(eligibles_last_seen_home=UNKNOWN_OCCUPIED)
-        self.household_structure.enumerated = True
-        self.household_structure.save()
-        form = HouseholdAssessmentForm(data=self.data)
-        self.assertFalse(form.is_valid())
-        errors = ''.join(form.errors.get('__all__'))
-        self.assertIn('Form is not required. Household has already been enumerated.', errors)
