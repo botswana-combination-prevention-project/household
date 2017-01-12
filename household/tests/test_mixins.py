76f35a5ac1b679eded11e0f399d9d05c100cc96f
erikvw <ew2789@gmail.com>
2017-01-12 20:14:56 +0200

    add tests to verify survey attrs

--- household/tests/test_mixins.py
+++ household/tests/test_mixins.py
@@ -28,6 +28,45 @@ class HouseholdMixin(HouseholdTestMixin):
         super().setUp()
         self.study_site = '40'
 
+    def is_survey_schedule(self, survey_schedule):
+        """Verifies the survey schedule object is valid.
+
+        Does not have to be a current survey schedule."""
+        if survey_schedule:
+            survey_schedule = site_surveys.get_survey_schedule_from_field_value(
+                survey_schedule.field_value)
+            if not survey_schedule:
+                raise TestMixinError(
+                    'Invalid survey specified. Got {}. See TestCase {} '
+                    'Expected one of {}'.format(
+                        survey_schedule.field_value,
+                        self.__class__.__name__,
+                        [s.field_value for s in site_surveys.get_survey_schedules()]))
+        return survey_schedule
+
+    def get_survey_schedule(self, name=None, index=None, field_value=None,
+                            group_name=None, current=None):
+        """Returns a survey schedule object.
+
+        You can also just use site_surveys!
+
+            * name: survey schedule name, e.g. 'bcpp_year.example-year-1'
+            * field_value: survey schedule field_value, e.g. 'bcpp_year.example-year-1.test_community'
+            * group_name: survey schedule group_name, e.g. 'bcpp_year'.
+            * index: list index of the ordered list of survey schedules
+            * current: if current=True only return a survey schedule if it is a current.
+                       If group_name is None, current defaults to True"""
+        if name:
+            survey_schedule = site_surveys.get_survey_schedule(name=name)
+            if current:
+                survey_schedule = survey_schedule if survey_schedule.current else None
+        else:
+            current = current if group_name else True
+            survey_schedules = site_surveys.get_survey_schedules(
+                group_name=group_name, current=current)
+            survey_schedule = survey_schedules[index or 0]
+        return survey_schedule
+
     def make_household_log_entry(self, household_log, household_status=None, **options):
         """Makes an householdlogentry instance.
 
@@ -53,19 +92,6 @@ class HouseholdMixin(HouseholdTestMixin):
             'household.householdrefusal',
             report_datetime=self.get_utcnow(),
             household_structure=household_structure)
-
-    def is_survey_schedule(self, survey_schedule):
-        if survey_schedule:
-            survey_schedule = site_surveys.get_survey_schedule_from_field_value(
-                survey_schedule.field_value)
-            if not survey_schedule or not survey_schedule.current:
-                raise TestMixinError(
-                    'Invalid survey specified. Got {}. See {} '
-                    'Expected one of {}'.format(
-                        survey_schedule.field_value,
-                        [s.field_value for s in site_surveys.get_survey_schedules(current=True)],
-                        self.__class__.__name__))
-        return survey_schedule
 
     def make_household_without_household_log_entry(self, survey_schedule=None):
         # note: accepts a survey schedule object not string
