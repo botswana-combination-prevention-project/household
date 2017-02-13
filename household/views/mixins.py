ff1b2030777dc737461677eb2b3aa3675c1643a5
erikvw <ew2789@gmail.com>
2017-02-13 08:59:34 +0200

    change from get to get_context_data override

--- household/views/mixins.py
+++ household/views/mixins.py
@@ -13,91 +13,144 @@ from .wrappers import (
 
 class HouseholdViewMixin:
 
-    household_wrapper_class = HouseholdModelWrapper
+    household_model_wrapper_class = HouseholdModelWrapper
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.household = None
+        self.household_wrapped = None
         self.household_identifier = None
 
-    def get(self, request, *args, **kwargs):
+    def get_context_data(self, **kwargs):
         """Add household to the view instance.
         """
+        context = super().get_context_data(**kwargs)
         self.household_identifier = kwargs.get('household_identifier')
-        try:
-            household = Household.objects.get(
+        if self.household_identifier:
+            self.household = Household.objects.get(
                 household_identifier=self.household_identifier)
-        except Household.DoesNotExist:
-            self.household = None
-        else:
-            self.household = self.household_wrapper_class(household)
-        kwargs['household'] = self.household
-        return super().get(request, *args, **kwargs)
+            self.household_wrapped = self.household_model_wrapper_class(
+                self.household)
+        context.update(household=self.household_wrapped)
+        return context
 
 
 class HouseholdStructureViewMixin:
 
-    household_structure_wrapper_class = HouseholdStructureModelWrapper
+    household_structure_model_wrapper_class = HouseholdStructureModelWrapper
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.household_structure = None
-        self.household_structures = None
+        self._household_structure = None
         self.survey_schedules_enumerated = []
 
-    def get(self, request, *args, **kwargs):
-        self.household_structures = HouseholdStructure.objects.filter(
-            household__id=self.household.id).order_by('enumerated_datetime')
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
         for household_structure in self.household_structures:
             if household_structure.enumerated:
                 self.survey_schedules_enumerated.append(
                     household_structure.survey_schedule_object)
-        try:
-            household_structure = HouseholdStructure.objects.get(
-                household__id=self.household.id,  # use id, model is wrapped
-                survey_schedule=self.survey_schedule_object.field_value)
-        except HouseholdStructure.DoesNotExist:
-            self.household_structure = None
-        else:
-            self.household_structure = self.household_structure_wrapper_class(
-                household_structure)
-        kwargs['household_structure'] = self.household_structure
-        kwargs['household_structures'] = self.household_structures
-        kwargs[
-            'survey_schedules_enumerated'] = self.survey_schedules_enumerated
-        return super().get(request, *args, **kwargs)
+        context.update(
+            household_structure=self.household_structure_wrapped,
+            household_structures=self.household_structures,
+            survey_schedules_enumerated=self.survey_schedules_enumerated)
+        return context
+
+    @property
+    def household_structure(self):
+        """Returns a household structure model instance or None.
+        """
+        if not self._household_structure:
+            try:
+                self._household_structure = HouseholdStructure.objects.get(
+                    household=self.household,
+                    survey_schedule=self.survey_schedule_object.field_value)
+            except HouseholdStructure.DoesNotExist:
+                self._household_structure = None
+        return self._household_structure
+
+    @property
+    def household_structure_wrapped(self):
+        """Returns a wrapped household structure.
+        """
+        return self.household_structure_model_wrapper_class(
+            self.household_structure)
+
+    @property
+    def household_structures(self):
+        """Returns a Queryset.
+        """
+        return HouseholdStructure.objects.filter(
+            household=self.household).order_by('survey_schedule')
 
 
 class HouseholdLogEntryViewMixin:
 
-    household_log_entry_wrapper_class = HouseholdLogEntryModelWrapper
+    household_log_entry_model_wrapper_class = HouseholdLogEntryModelWrapper
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.household_log = None
-        self.household_log_entry = None
-        self.household_log_entries = None
-        self.current_household_log_entry = None
+        self._current_household_log_entry = None
 
-    def get(self, request, *args, **kwargs):
-        """Add household to the view instance.
+    def get_context_data(self, **kwargs):
+        """Add household log, log entry and log entries to the context.
         """
-        self.household_log = self.household_structure.wrapped_object.householdlog
-        objs = (
-            self.household_structure.wrapped_object.householdlog.householdlogentry_set.all())
-        self.household_log_entries = [
-            self.household_log_entry_wrapper_class(obj)
-            for obj in objs.all().order_by('-report_datetime')]
+        context = super().get_context_data(**kwargs)
+        context.update(
+            household_log=self.household_log,
+            household_log_entries=self.household_log_entries_wrapped,
+            current_household_log_entry=self.current_household_log_entry_wrapped)
+        return context
+
+    @property
+    def household_log(self):
+        if self.household_structure:
+            return self.household_structure.householdlog
+        return None
+
+    @property
+    def current_household_log_entry_wrapped(self):
+        """Returns a model wrapper instance.
+        """
+        return (
+            self.household_log_entry_model_wrapper_class(
+                self.current_household_log_entry
+                or HouseholdLogEntry(
+                    household_log=self.household_log))
+        )
+
+    @property
+    def current_household_log_entry(self):
+        """Returns a household log entry model instance or None.
+        """
+        if not self._current_household_log_entry:
+            try:
+                obj = todays_log_entry_or_raise(
+                    self.household_structure, report_datetime=get_utcnow())
+            except HouseholdLogRequired:
+                obj = None
+            self._current_household_log_entry = obj
+        return self._current_household_log_entry
+
+    @property
+    def household_log_entries(self):
+        """Returns a household_log_entry Queryset.
+        """
         try:
-            self.current_household_log_entry = todays_log_entry_or_raise(
-                self.household_structure.wrapped_object, report_datetime=get_utcnow())
-        except HouseholdLogRequired:
-            self.current_household_log_entry = HouseholdLogEntry(
-                household_log=self.household_log)
-        self.current_household_log_entry = self.household_log_entry_wrapper_class(
-            self.current_household_log_entry)
-        kwargs['household_log'] = self.household_log
-        kwargs['household_log_entries'] = self.household_log_entries
-        kwargs[
-            'current_household_log_entry'] = self.current_household_log_entry
-        return super().get(request, *args, **kwargs)
+            return (
+                self.household_structure.householdlog.householdlogentry_set
+                .all().order_by('-report_datetime'))
+        except AttributeError as e:
+            if 'householdlog' not in str(e) and 'householdlogentry_set' not in str(e):
+                raise
+            return HouseholdLogEntry.objects.none()
+
+    @property
+    def household_log_entries_wrapped(self):
+        """Returns a list of household_log_entry model wrappers.
+        """
+        wrapped_objects = []
+        for obj in self.household_log_entries:
+            wrapped_objects.append(
+                self.household_log_entry_model_wrapper_class(obj))
+        return wrapped_objects
