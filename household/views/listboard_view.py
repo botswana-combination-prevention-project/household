68939445d713431f2cda8f57c3ed822a057cae56
Coulson <ckgathi@bhp.org.bw>
2017-03-04 08:32:13 +0200

    query filter by machine

--- household/views/listboard_view.py
+++ household/views/listboard_view.py
@@ -1,3 +1,5 @@
+import socket
+
 from django.apps import apps as django_apps
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
@@ -27,10 +29,11 @@ class ListboardView(SurveyViewMixin, EdcBaseViewMixin, AppConfigViewMixin,
 
     def get_queryset_filter_options(self, request, *args, **kwargs):
         options = super().get_queryset_filter_options(request, *args, **kwargs)
+        device_name = socket.gethostname()
         plot_identifier_list = []
         try:
             plot_identifier_list = InnerContainer.objects.get(
-                username=request.user.username).identifier_labels
+                device_name=device_name).identifier_labels
         except InnerContainer.DoesNotExist:
             plot_identifier_list = []
         if plot_identifier_list:
